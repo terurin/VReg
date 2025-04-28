ae04baf0e44ff0cbf3fe12b5f302a752475ba3a7
terurin <teru@soum.co.jp>
2025-04-29 02:33:28 +0900

    vregex: add varchar

--- vreg/inc/vregex.hpp
+++ vreg/inc/vregex.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include <algorithm>
 #include <bit>
+#include <compare>
 #include <concepts>
 #include <cstddef>
 #include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string_view>
 namespace vregex {
 
 #if __cplusplus < 202302L
@@ -19,4 +23,76 @@ template <std::integral T> constexpr T byteswap(T x) {
 #else
 using std::byteswap;
 #endif
+
+// NOTE: heapless string class like SQL's VARCHAR(n)
+template <size_t cap> class varchar {
+    constexpr static size_t capacity_ = cap;
+    using traits = std::char_traits<char>;
+    char chars_[cap + 1];
+
+public:
+    constexpr varchar() { memset(chars_, 0, sizeof(chars_)); }
+
+    constexpr varchar(const char *s) {
+        assert(s);
+        const size_t l = strnlen(s, cap);
+        memcpy(chars_, s, l);
+        memset(chars_ + l, 0, sizeof(chars_) - l);
+    }
+    constexpr varchar(std::string_view sv) { sv.copy(chars_, cap); }
+    template <size_t cap2> constexpr varchar(const varchar<cap2> &v) {
+        const size_t l = std::min(v.size(), cap);
+        memcpy(chars_, v.data(), l);
+        memset(chars_ + l, 0, sizeof(chars_) - l);
+    }
+    constexpr varchar(varchar<cap> &&) = default;
+    auto operator<=>(const varchar<cap> &) const = default;
+    constexpr operator std::string_view() const { return chars_; }
+
+    // iterator
+    constexpr char *begin() { return chars_; }
+    constexpr const char *cbegin() const { return chars_; }
+    constexpr char *end() { return chars_ + size(); }
+    constexpr const char *cend() const { return chars_ + size(); }
+
+    // region
+    constexpr size_t max_size() const { return cap; }
+    constexpr size_t size() const { return traits::length(chars_); }
+    constexpr size_t length() const { return size(); }
+    constexpr bool empty() const { return chars_[0]; }
+
+    // element
+    constexpr char at(size_t index) const { return index < capacity_ ? chars_[index] : 0; }
+    constexpr char operator[](size_t index) const { return index < sizeof(chars_) ? chars_[index] : 0; }
+    constexpr char &operator[](size_t index) {
+        assert(index < sizeof(chars_));
+        return chars_[index];
+    }
+    constexpr const char *data() const { return chars_; }
+    constexpr const char *c_str() const { return chars_; }
+
+    // modify
+    constexpr varchar &operator=(std::string_view sv) {
+        const size_t l = std::min(sv.size(), cap);
+        memcpy(chars_, sv.data(), l);
+        memset(chars_ + l, 0, sizeof(chars_) - l);
+        return *this;
+    }
+    constexpr varchar &operator+=(std::string_view sv) {
+        const size_t n = size();
+        const size_t writable = std::min(sv.size(), max_size() - n);
+        strncpy(chars_ + n, sv.data(), writable);
+        return *this;
+    }
+};
+template <size_t n> std::ostream &operator<<(std::ostream &os, const varchar<n> &v) { return os << v.c_str(); }
+
+using varchar_short = varchar<7>;
+using varchar_middle = varchar<127>; 
+using varchar_long = varchar<255>;
+
 }; // namespace vregex
+
+template <size_t n> struct std::hash<vregex::varchar<n>> {
+    size_t operator()(const vregex::varchar<n> &v) const { return std::hash<const char *>()(v.c_str()); }
+};
--- vreg/test/vregex_test.cpp
+++ vreg/test/vregex_test.cpp
@@ -3,10 +3,39 @@
 #include <vregex.hpp>
 using namespace vregex;
 
-TEST(vregex, byteswap) {
+TEST(byteswap, byteswap) {
     EXPECT_EQ(byteswap<bool>(true), true);
     EXPECT_EQ(byteswap<uint8_t>(0x81), 0x81);
     EXPECT_EQ(byteswap<uint16_t>(0x1122), 0x2211);
     EXPECT_EQ(byteswap<uint32_t>(0x11223344), 0x44332211);
     EXPECT_EQ(byteswap<uint64_t>(0x1122334455667788UL), 0x8877665544332211UL);
+}
+
+TEST(varchar, adjust_size) {
+
+    const varchar<7> eight("12345678");
+    const varchar<7> seven("1234567");
+    EXPECT_EQ(eight, seven);
+    EXPECT_EQ((std::string_view)eight, "1234567");
+    EXPECT_EQ(eight.size(), 7);
+    EXPECT_EQ(eight.max_size(), 7);
+
+    const varchar<3> three(seven);
+    EXPECT_EQ(three, "123");
+    EXPECT_EQ(three.size(), 3);
+    EXPECT_EQ(three.max_size(), 3);
+}
+
+TEST(varchar, iterator) {
+    varchar<7> eight("1234567");
+    EXPECT_EQ(*eight.cbegin(), '1');
+    EXPECT_EQ(*(eight.cend() - 1), '7');
+    EXPECT_EQ(*eight.cend(), '\0');
+}
+
+TEST(varchar, modify) {
+    varchar<7> x = "1";
+    x += "2";
+    const varchar<7> y = "12";
+    EXPECT_TRUE(x== y);
 }
